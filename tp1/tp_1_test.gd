89274cfc79afb1fb3dc88390279fd39046dcd17d
atplab <154367090+atplab@users.noreply.github.com>
2025-09-05 11:41:25 -0400

    tp1_5sept_11h41

--- tp1/tp_1_test.gd
+++ tp1/tp_1_test.gd
@@ -7,7 +7,39 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		$playerwalking.play("walk")
 	else:
-		$playerwalking.play("idle")
+		if Input.is_action_pressed("ui_left"):
+			$playerwalking.play("walk_left")
+		else:
+			$playerwalking.play("idle")
+			
+	
+	
+		
+	if Input.is_action_pressed("ui_up"):
+		$playerjump.play("playerjump")
+	else:
+		$playerjump.play("idle_jump")
+		
+	if Input.is_action_pressed("ui_page_down"):
+		$"red ennemy".play("Red_walk")
+	else:
+		$"red ennemy".play("Red_Idle")
+		
+	if Input.is_action_pressed("ui_page_up"):
+		$"Flying bee".play("Bee_flying")
+		$"Flying bee/Flying_bee".play("Bee_flying")
+		$coin_bronze.play("default")
+		$coin_bronze/coin_bronze2.play("default")
+		$coin_bronze/coin_bronze3.play("default")
+		$coin_bronze/coin_bronze4.play("default")
+		$coin_bronze/coin_bronze5.play("default")
+		$coin_bronze/coin_bronze6.play("default")
+		$coin_bronze/coin_bronze.play("default")
+		$coin_argent/coin_argent2.play("default")
+		$coin_argent.play("default")
+		$spring.play("default")
+		$spring/spring.play("default")
+		$spring/spring2.play("default")
 
 
 # Exemple : touche -> son + animation
