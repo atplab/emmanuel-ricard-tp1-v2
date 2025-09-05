270f0358499ea695af83aa39dac3567c13365bdd
atplab <154367090+atplab@users.noreply.github.com>
2025-09-05 10:03:55 -0400

    5sept 10h

--- tp1/tp_1_test.gd
+++ tp1/tp_1_test.gd
@@ -2,6 +2,12 @@ extends Node2D
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		print("bouton pressé")
+		
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		$playerwalking.play("walk")
+	else:
+		$playerwalking.play("idle")
 
 
 # Exemple : touche -> son + animation
